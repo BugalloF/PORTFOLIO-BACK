0ce0c2dac640292ed31864ff33eaa2db2ace0164
BugalloF <bugallof@gmail.com>
2023-05-27 19:19:16 -0300

    Adminer modified

--- data/admin.py
+++ data/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Skills,ProjectsImages,Projects,Education,JobExperience
 from django.contrib.auth.models import Group,User
 # Register your models here.
-admin.site.register(Skills)
-class ProjectsAdmin(admin.ModelAdmin):
-    list_display = ('started_formatted','ended_formatted')
 
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('title','started_formatted','ended_formatted','language')
+    list_filter=('language',)
+    # filter_horizontal=('projectImages',)
     def started_formatted(self, obj):
         return obj.started.strftime('%d/%m/%Y')
     
@@ -14,13 +15,34 @@ class ProjectsAdmin(admin.ModelAdmin):
             return obj.ended.strftime('%d/%m/%Y')
         else:
             return obj.ended
+    
 
     started_formatted.short_description = 'Started'
-    ended_formatted.short_description = 'Started'
-admin.site.register(Projects)
+    ended_formatted.short_description = 'Ended'
+
+
+class SkillsAdmin(admin.ModelAdmin):
+    list_display=('title','is_soft_skill','language')
+    list_filter=('language','is_soft_skill')
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display=('degree','language','started_formatted','ended_formatted')
+    list_filter=('language',)
+    def started_formatted(self, obj):
+        return obj.started.strftime('%d/%m/%Y')
+    
+    def ended_formatted(self, obj):
+        if obj.ended :
+            return obj.ended.strftime('%d/%m/%Y')
+        else:
+            return obj.ended
+    started_formatted.short_description = 'Started'
+    ended_formatted.short_description = 'Ended'
+admin.site.register(Projects,ProjectsAdmin)
 admin.site.register(ProjectsImages)
-admin.site.register(Education)
+admin.site.register(Education,EducationAdmin)
 admin.site.register(JobExperience)
+admin.site.register(Skills,SkillsAdmin)
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
